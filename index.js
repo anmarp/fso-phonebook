fc016966104482199919ac93a85c42b659951c59
anmarp <67588125+anmarp@users.noreply.github.com>
2020-09-26 14:18:35 +0300

    Add solution to exercise 3.2

--- index.js
+++ index.js
@@ -3,30 +3,33 @@ const app = express()
 
 let persons = [
   {
-    "persons": [
-      {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-      },
-      {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-      },
-      {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-      },
-      {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-      }
-    ]
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
   }
 ]
+
+const info = `<p>Phonebook has info for ${persons.length} people</p>`
+const time = new Date()
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>` + new Date())
+})
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
