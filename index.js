6bc9a96c0d114bfec36e157427e6f82665bc73d9
anmarp <67588125+anmarp@users.noreply.github.com>
2020-09-26 20:54:01 +0300

    Add Procfile and change PORT

--- index.js
+++ index.js
@@ -83,12 +83,13 @@ app.post('/api/persons', (request, response) => {
     name: body.name,
     number: body.number
   }
-  
+
   persons = persons.concat(person)
 
   response.json(person)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
