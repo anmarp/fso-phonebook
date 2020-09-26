015281df2fc9db185179073adfe8f960e7f7b5ba
anmarp <67588125+anmarp@users.noreply.github.com>
2020-09-26 15:00:50 +0300

    Add solutions to exercises 3.3-3.4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -24,15 +25,30 @@ let persons = [
   }
 ]
 
-const info = `<p>Phonebook has info for ${persons.length} people</p>`
-const time = new Date()
-
-app.get('/info', (req, res) => {
-  res.send(`<p>Phonebook has info for ${persons.length} people</p>` + new Date())
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>` + new Date())
 })
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
