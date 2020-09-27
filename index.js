b3d559daed851018fff6cc4a6367c87209b7ff52
anmarp <67588125+anmarp@users.noreply.github.com>
2020-09-27 19:40:47 +0300

    Add solutions to exercises 3.16-3.18

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
+const { request, response } = require('express')
 
 app.use(cors())
 app.use(express.json())
@@ -17,10 +18,10 @@ morgan.token('req-body', (request, response) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
-let persons = []
-
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>` + new Date())
+  Person.find({}).then(persons => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>` + new Date())
+  })  
 })
 
 app.get('/api/persons', (request, response) => {
@@ -29,32 +30,32 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params._id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  console.log(request.params)
-  console.log(request.params.id)
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-
+  
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: 'namer or number missing'
+      error: 'Name or number missing'
     })
   }
 
@@ -68,7 +69,41 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'Unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformed id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
